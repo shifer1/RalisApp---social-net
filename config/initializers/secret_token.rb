8e51b16c296741f5ab90e482cd6792c268a3884c
Saeed <saeed.gback@gmail.com>
2015-09-28 00:37:59 +0330

    Create newApps and add satatus table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApps::Application.config.secret_token = 'c5ecec9d77ada60a4d999bc23e81c7e1200e67dff14c5f933074970f7209ac0de8488b0b7dcdbe91425f8a1d121f86aee0634597b3611b5c8f22048c7467b9fc'
